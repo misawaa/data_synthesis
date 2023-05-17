ad0fa6a0c49e0e96bbb5570b47e86850b2937b78
misawaa <108860285+misawaa@users.noreply.github.com>
2023-05-17 11:45:21 +0700

    remove duplicates from sorted arrays

---
+++ python/leetcode/easy/removeDuplicates.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        count = 0
+        
+        # duyet nums
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i-1]:
+
+                nums[count] = nums[i]
+
+                count += 1
+        return count
