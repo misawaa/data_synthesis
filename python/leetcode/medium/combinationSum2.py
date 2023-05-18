0853f73a724d7e32ffaeb40005765f5165ba332a
misawaa <108860285+misawaa@users.noreply.github.com>
2023-05-18 13:16:55 +0700

    combination sum II

---
+++ python/leetcode/medium/combinationSum2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(start, curr_sum, chosen):
+            if curr_sum == target:
+                result.append(list(chosen))
+                return
+            
+            if curr_sum > target:
+                return
+            
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                chosen.append(candidates[i])
+                backtrack(i+1, curr_sum + candidates[i], chosen)
+                chosen.pop()
+        
+        result = []
+        candidates.sort()
+        backtrack(0, 0, [])
+        return result
